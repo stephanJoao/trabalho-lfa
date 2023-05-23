25d7e9230eb37ba297ae484c12dc6c109ea3740d
João Stephan <joaossmauricio@gmail.com>
2023-05-23 15:05:29 -0300

    commit inicial

---
+++ java/Main.java
@@ -0,0 +1,32 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    Main t = new Main();		
+       t.faca1("ababa");
+       // t.faca2();
+    }
+    
+    public void faca1(String w) {
+        AFD a = new AFD();
+        try {
+               a.ler("./AFD.xml");
+               System.out.println("AFD M = "+a);
+               if (a.Aceita(w))
+                   System.out.println("Aceitou "+w);
+               System.out.println("Pe(q0,"+w+"):"+a.pe(a.getEstadoInicial(),w));
+        } catch (Exception e){
+               System.out.println(e); 
+        } 
+    }
+
+    public void faca2() {
+        AFN a = new AFN();
+        try {
+               a.ler("./AFN01.xml");
+               System.out.println("AFN M = "+a);
+               System.out.println(("AFD M' = " + a.toAFD()).toString());
+        } catch (Exception e){
+               System.out.println(e);
+        }     
+    }
+}
